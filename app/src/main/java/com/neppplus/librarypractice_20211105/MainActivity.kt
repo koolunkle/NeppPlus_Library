ac72c91f07b52eabf3c1b0323b5db4b6b827546f
김준기 <gugi329@daum.net>
2021-11-09 22:26:37 +0900

    TedPermission 권한 획득 작업중   - 라이브러리 설치 (버젼 3.3.0 확인)   - Manifest에 uses-permission 태그 명시   - 권한 획득/거절 여부에 따른 행동 방침 작성중    => 거절되었을때 토스트 띄우기만.

--- app/src/main/java/com/neppplus/librarypractice_20211105/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20211105/MainActivity.kt
@@ -3,6 +3,8 @@ package com.neppplus.librarypractice_20211105
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +16,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupEvents() {
+
+        btnCall.setOnClickListener {
+
+//            Intent (4) - ACTION_CALL 활용.
+
+//            권한 획득.
+//            1) 권한 여하에 따른 상황별 대처 방안. 미리 변수에 담아두자.
+
+            val permissionListener =  object : PermissionListener {
+
+                override fun onPermissionGranted() {
+
+//                    권한이 최종 획득 OK 일때 실행할 코드.
+
+
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+//                    최종 권한 거부 되었을때.
+                    Toast.makeText(this@MainActivity, "권한이 거부되어 전화 연결이 불가능합니다.", Toast.LENGTH_SHORT)
+                        .show()
+
+                }
+
+            }
+
+
+
+        }
+
 
 //        이미지뷰 / 텍스트뷰 / LinearLayout 등은 버튼처럼 setOnClickListener 가능.
 
