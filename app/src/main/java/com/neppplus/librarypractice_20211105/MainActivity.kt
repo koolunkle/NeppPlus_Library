5ff48bc2b9ae2fd54d09bb6254d7bbe988fbcbd8
김준기 <gugi329@daum.net>
2021-11-09 22:17:25 +0900

    프로필 사진 누르면 -> 크게 보기 화면으로 이동   - 크게 보는 화면만 액션바 제거

--- app/src/main/java/com/neppplus/librarypractice_20211105/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20211105/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.neppplus.librarypractice_20211105
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+
+//        이미지뷰 / 텍스트뷰 / LinearLayout 등은 버튼처럼 setOnClickListener 가능.
+
+        imgProfile.setOnClickListener {
+
+            val myIntent = Intent(this, ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
+    }
+
+    fun setValues() {
+
+    }
+
 }
---
+++ app/src/main/java/com/neppplus/librarypractice_20211105/ViewPhotoActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.librarypractice_20211105
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_photo)
+    }
+}
